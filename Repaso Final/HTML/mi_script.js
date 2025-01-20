5c3a26129ae8e5bbfd751854ad138ed13cd09515
GuadaDiaz <127156542+GuadaDiaz@users.noreply.github.com>
2025-01-20 20:44:44 -0300

    Delete Repaso Final/HTML directory

--- Repaso Final/HTML/mi_script.js
+++
@@ -1,6 +0,0 @@
-function Calcular(){
-    let nombre = document.getElementById("nombre").value;
-    let edad = document.getElementById("nacimiento").value;
-
-    document.getElementById("Resultado").innerHTML = nombre + " tiene " + edad + " años";
-}
